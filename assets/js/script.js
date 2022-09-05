758f9e17e94158032285a8d7e3109500727733f6
Your Name <you@example.com>
2022-09-04 23:31:53 -0400

    updated five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,30 @@ var citySearchTermEl = document.querySelector("#city-search-term");
 var currentWeatherEl = document.querySelector("#current-weather");
 var displayCityEl = document.querySelector("#city");
 var currentDayEl = document.getElementById("currentDay");
+var todaysDateEl = document.getElementById("todaysDate");
+var searchFormHistory = document.getElementById("recent-search");
 
+recentSearch = [""];
 
+const apiKey = "c6a9bf78cf3b504fe7e8382ca53765c4";
+
+// Created the dates to  list for each weather card. 
 var todaysDate = moment();
 currentDayEl.textContent = todaysDate.format("LLLL");
 
-const apiKey = "c6a9bf78cf3b504fe7e8382ca53765c4";
+var datesListed = moment();
+todaysDateEl.textContent = datesListed.format("ll");
+let tomorrow = document.querySelector("#tempOne-day");
+tomorrow.textContent = moment().add(1, "days").format("ll");
+let tomorrow2 = document.querySelector("#tempTwo-day");
+tomorrow2.textContent = moment().add(2, "days").format("ll");
+let tomorrow3 = document.querySelector("#tempThree-day");
+tomorrow3.textContent = moment().add(3, "days").format("ll");
+let tomorrow4 = document.querySelector('#tempFour-day');
+tomorrow4.textContent = moment().add(4, "days").format("ll");
+let tomorrow5 = document.querySelector('#tempFive-day');
+tomorrow5.textContent = moment().add(5, "days").format("ll");
+
 
 //calling the API function to get the lat and lon values.
 var retrieveUsersInput = function (city) {
@@ -29,6 +47,7 @@ var retrieveUsersInput = function (city) {
           }
         });
 };
+
 // Retrieve weather; daily and forecast.
 var getWeather = function (lat, lon) {
     var apiUrl = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
@@ -41,18 +60,48 @@ var getWeather = function (lat, lon) {
             //get current weather first; temp, wind, humid, uv index
                 let currentTemp = document.querySelector("#currentTemp");
                 currentTemp.innerHTML = data.current.temp;
-                console.log(currentTemp);
                 let currentWindEl = document.querySelector("#currentWind");
                 currentWindEl.innerHTML = data.current.wind_speed;
                 let currentHumidityEl = document.querySelector("#currentHumidity");
                 currentHumidityEl.innerHTML = data.current.humidity;
                 let currentUvIndexEl = document.querySelector("#currentUvIndex");
                 currentUvIndexEl.innerHTML = data.current.uvi;
-            
-            // get forecast - repeat like current weather create divs and create ids
-            // var temp1 = document.querySelector("#temp
-            // temp1.innerHTML = data.daily[1].
-                //return
+
+            // get id=fiveday-weather-cards
+                let tempOneTempEl = document.querySelector("#tempOne-temp");
+                tempOneTempEl.innerHTML = data.daily[2];
+                let tempOneWindEl = document.querySelector("#tempOne-wind");
+                tempOneWindEl.innerHTML = data.daily[3];
+                let tempOneHumidityEl = document.querySelector("#tempOne-humidity");
+                tempOneHumidityEl.innerHTML = data.daily[4]
+
+                let tempTwoTempEl = document.querySelector("#tempTwo-temp");
+                tempTwoTempEl.innerHTML = data.daily[2];
+                let tempTwoWindEl = document.querySelector("#tempTwo-wind");
+                tempTwoWindEl.innerHTML = data.daily[3];
+                let tempTwoHumidityEl = document.querySelector("#tempTwo-humidity");
+                tempTwoHumidityEl.innerHTML = data.daily[4]
+
+                let tempThreeTempEl = document.querySelector("#tempThree-temp");
+                tempThreeTempEl.innerHTML = data.daily[2];
+                let tempThreeWindEl = document.querySelector("#tempThree-wind");
+                tempThreeWindEl.innerHTML = data.daily[3];
+                let tempThreeHumidityEl = document.querySelector("#tempThree-humidity");
+                tempThreeHumidityEl.innerHTML = data.daily[4]
+
+                let tempFourTempEl = document.querySelector("#tempFour-temp");
+                tempFourTempEl.innerHTML = data.daily[2];
+                let tempFourWindEl = document.querySelector("#tempFour-wind");
+                tempFourWindEl.innerHTML = data.daily[3];
+                let tempFourHumidityEl = document.querySelector("#tempFour-humidity");
+                tempFourHumidityEl.innerHTML = data.daily[4]
+
+                let tempFiveTempEl = document.querySelector("#tempFive-temp");
+                tempFiveTempEl.innerHTML = data.daily[2];
+                let tempFiveWindEl = document.querySelector("#tempFive-wind");
+                tempFiveWindEl.innerHTML = data.daily[3];
+                let tempFiveHumidityEl = document.querySelector("#tempFive-humidity");
+                tempFiveHumidityEl.innerHTML = data.daily[4]
 
                 displayWeather(data);
             })
@@ -61,7 +110,6 @@ var getWeather = function (lat, lon) {
           }
         });
 };
-
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -80,5 +128,7 @@ userFormEl.addEventListener("submit", formSubmitHandler);
 
 var displayWeather = function(data) {
     console.log(data);
-    currentWeatherEl.textContent = "Todays Weather is...";
   };
+
+var searchHistoryLog = function() {
+}
